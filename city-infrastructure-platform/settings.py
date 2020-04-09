95b86ba5face454f1c32926735546afeda535fbb
Mika Hietanen <m.hietanen84@gmail.com>
2020-04-09 14:26:49 +0300

    Add default authentication classes
    Refs #LIIK-89

--- city-infrastructure-platform/settings.py
+++ city-infrastructure-platform/settings.py
@@ -174,6 +174,11 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 
 # Django REST Framework
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "helusers.oidc.ApiTokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "PAGE_SIZE": 10,
