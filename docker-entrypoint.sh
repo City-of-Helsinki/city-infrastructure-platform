47bbf3f1bf7c819fe41efa8c94e87524e2733bc9
Mika Hietanen <m.hietanen84@gmail.com>
2020-05-12 17:21:45 +0300

    Add more startup-logging
    Add more startup-logging.
    Temporarily disable compilemessages due to Azure-issues.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,16 +15,18 @@ if [[ "$COLLECT_STATIC" = "1" ]]; then
 fi
 
 # Only translate traffic_control module for now
-echo "Updating translations..."
-cd traffic_control
-../manage.py compilemessages -l fi
-cd ..
+# echo "Updating translations..."
+# cd traffic_control
+# ../manage.py compilemessages -l fi
+# cd ..
 
 # Start server
 if [[ ! -z "$@" ]]; then
     "$@"
 elif [[ "$DEV_SERVER" = "1" ]]; then
+    echo "Starting dev-server..."
     python ./manage.py runserver 0.0.0.0:8000
 else
+    echo "Starting uwsgi-server..."
     uwsgi --ini uwsgi_docker.ini
 fi
