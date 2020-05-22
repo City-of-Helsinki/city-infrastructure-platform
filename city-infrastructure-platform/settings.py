41bda7c5ad522b3b1ae1415dc0df10d5829bf704
Mika Hietanen <m.hietanen84@gmail.com>
2020-05-22 07:51:17 +0300

    Increase pagination page size

--- city-infrastructure-platform/settings.py
+++ city-infrastructure-platform/settings.py
@@ -188,7 +188,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 100,
 }
 
 # django-cors
