6016358ce5a2ed19202e56c6b0f59cb49677e1b3
Eetu Häivälä <eetu.haivala@anders.fi>
2020-07-07 10:03:36 +0300

    Exclude legacy_code from AdditionalSignReal API
    legacy_code should not have been exposed in the API in the first place.
    The value is used only for imports and it should be of no interest for
    end users.

--- traffic_control/serializers.py
+++ traffic_control/serializers.py
@@ -222,7 +222,7 @@ class AdditionalSignRealSerializer(
             "deleted_by",
             "deleted_at",
         )
-        exclude = ("mount_type", "is_active", "deleted_at", "deleted_by")
+        exclude = ("mount_type", "legacy_code", "is_active", "deleted_at", "deleted_by")
 
 
 class AdditionalSignRealGeoJSONSerializer(AdditionalSignRealSerializer):
