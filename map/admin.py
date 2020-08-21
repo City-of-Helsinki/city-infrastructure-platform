1e029c969a62988864ff1909ac5c03084b3310c1
Mingfeng Yu <mfdevworld@gmail.com>
2020-08-21 11:45:25 +0300

    Add model admin for Layer model
    This allows admins to add layer to map view at a
    later time
    
    Refs: LIIK-78

--- map/admin.py
+++ map/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from map.models import Layer
+
+
+@admin.register(Layer)
+class LayerAdmin(admin.ModelAdmin):
+    list_display = ("identifier", "name_fi", "name_en", "is_basemap", "order")
+    search_fields = ("identifier", "name_fi", "name_en")
+    list_filter = ("is_basemap",)
