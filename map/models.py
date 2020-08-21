80d377e97841d161d28f21bc5e164ca799fb5ff7
Mingfeng Yu <mfdevworld@gmail.com>
2020-08-21 11:33:28 +0300

    Add a Layer model
    The layer model is to store layer details which are available
    in map view
    
    Refs: LIIK-78

--- map/models.py
+++ map/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Layer(models.Model):
+    identifier = models.CharField(_("identifier"), max_length=200)
+    name_fi = models.CharField(_("Finnish name"), max_length=200)
+    name_en = models.CharField(_("English name"), max_length=200)
+    is_basemap = models.BooleanField(_("is basemap"), default=False)
+    order = models.IntegerField(_("order"), default=1)
+
+    class Meta:
+        verbose_name = _("layer")
+        verbose_name_plural = _("layers")
+        ordering = ("is_basemap", "order")
+
+    def __str__(self):
+        return self.name_fi
