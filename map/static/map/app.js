75f9424bf35848a2482ceb4e7d74f95a2a82f761
Mingfeng Yu <mfdevworld@gmail.com>
2020-08-26 09:36:29 +0300

    Show links to admin change view when clicking features on map view
    The features information are fetched via WMS GetFeatureInfo requests.
    
    For simplicity purposes, we made following assumptions to the format
    of WMS GetFeatureInfo responses for all layers (base on current
    GeoServer setup):
    
    - All layers are traffic control layers
    - The "id" field of features are in the format of "<feature_type>.<instance_id>"
    - The <feature_type> must match the model contenttype name after removing all "_"
    
    Refs: LIIK-78

--- map/static/map/app.js
+++ map/static/map/app.js
@@ -5,24 +5,84 @@
   function MapView(target, mapConfig) {
     this.mapConfig = mapConfig;
     this.map = this.createMap(target);
+    this.featureInfoPopup = this.createFeatureInfoPopup();
+    this.map.addOverlay(this.featureInfoPopup);
   }
 
   MapView.prototype.createMap = function(target) {
     const projection = this.getProjection();
     const helsinkiCoords = [25499052.02, 6675851.38];
     const resolutions = [256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5, 0.25, 0.125, 0.0625];
-    return new ol.Map({
+    const basemapLayerGroup = this.getBasemapLayerGroup()
+    const overlayLayerGroup = this.getOverlayLayerGroup()
+    const view = new ol.View({
+      projection: projection,
+      center: helsinkiCoords,
+      zoom: 5,
+      resolutions: resolutions,
+      extent: projection.getExtent()
+    });
+    const map = new ol.Map({
       target: target,
       controls: this.getControls(),
-      layers: [this.getBasemapLayerGroup(), this.getOverlayLayerGroup()],
-      view: new ol.View({
-        projection: projection,
-        center: helsinkiCoords,
-        zoom: 5,
-        resolutions: resolutions,
-        extent: projection.getExtent()
-      })
+      layers: [basemapLayerGroup, overlayLayerGroup],
+      view: view
     });
+
+    map.on("singleclick", (event) => {
+      const viewResolution = view.getResolution();
+      const visibleLayers = overlayLayerGroup.getLayers().getArray().filter(layer => layer.getVisible())
+      if (visibleLayers.length > 0) {
+        const layerNames = visibleLayers.map(layer => layer.getSource().getParams().LAYERS);
+        const url = visibleLayers[0].getSource().getFeatureInfoUrl(
+          event.coordinate,
+          viewResolution,
+          projection,
+          {
+            INFO_FORMAT: "application/json",
+            LAYERS: layerNames.join(","),
+            FEATURE_COUNT: 10
+          }
+        );
+
+        if (url) {
+          fetch(url).then(response => response.text()).then(responseText => {
+            const data = JSON.parse(responseText);
+            const features = data["features"];
+            if (features.length > 0) {
+              this.showFeatureInfo(event.coordinate, features);
+            }
+          });
+        }
+      }
+    });
+
+    return map;
+  };
+
+  MapView.prototype.createFeatureInfoPopup = function() {
+    return new ol.Overlay({
+      element: document.getElementById("feature-info-popup"),
+      autoPan: true,
+      autoPanAnimation: {
+        duration: 250,
+      }
+    });
+  };
+
+  MapView.prototype.closeFeatureInfo = function() {
+    this.featureInfoPopup.setPosition(undefined);
+  };
+
+  MapView.prototype.showFeatureInfo = function(coordinate, features) {
+    const featureList = features.map(feature => {
+      const [featureType, featureId] = feature["id"].split(".");
+      const url = `/admin/traffic_control/${featureType.replace(/_/g, "")}/${featureId}/change`;
+      return `<div>${featureType}: <a target="_blank" href="${url}">${featureId}</a></div>`;
+    });
+    const featureListElem = document.getElementById("feature-list");
+    featureListElem.innerHTML = featureList.join("\n");
+    this.featureInfoPopup.setPosition(coordinate);
   };
 
   MapView.prototype.getBasemapLayerGroup = function() {
