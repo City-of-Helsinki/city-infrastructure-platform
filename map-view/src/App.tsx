2fdc25d54797116e960d232b0c56f2142f78f247
Mingfeng Yu <mfdevworld@gmail.com>
2020-08-28 14:23:06 +0300

    Format code with prettier

--- map-view/src/App.tsx
+++ map-view/src/App.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import './App.css';
-import Map from './common/Map';
-import MapConfigAPI from './api/MapConfigAPI';
+import React from "react";
+import "./App.css";
+import Map from "./common/Map";
+import MapConfigAPI from "./api/MapConfigAPI";
 
-import 'ol/ol.css';
+import "ol/ol.css";
 
 class App extends React.Component {
   mapId = "map";
 
   componentDidMount() {
-    MapConfigAPI.getMapConfig().then(mapConfig => Map.initialize(this.mapId, mapConfig));
+    MapConfigAPI.getMapConfig().then((mapConfig) => Map.initialize(this.mapId, mapConfig));
   }
 
   render() {
-    return (
-      <div id={this.mapId}></div>
-    );
+    return <div id={this.mapId}></div>;
   }
 }
 
