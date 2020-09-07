49ec97414177cbcfb0a4b7c32e951ec0210d81b2
Eetu Häivälä <eetu.haivala@anders.com>
2020-09-07 11:03:04 +0300

    Open attached files in new tab
    Implement custom `AdminFileWidget` that adds `target="_blank"` attribute
    for the `a` tag. Otherwise the template is identical with the template
    used by the default widget.
    
    Use the custom `AdminFileWidget` in file model inlines for any plan and
    real models.
    
    Refs LIIK-176

--- traffic_control/admin/barrier.py
+++ traffic_control/admin/barrier.py
@@ -1,7 +1,9 @@
 from django.contrib.gis import admin
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from ..constants import HELSINKI_LATITUDE, HELSINKI_LONGITUDE
+from ..forms import AdminFileWidget
 from ..mixins import (
     EnumChoiceValueDisplayAdminMixin,
     SoftDeleteAdminMixin,
@@ -19,6 +21,9 @@ __all__ = (
 
 
 class BarrierPlanFileInline(admin.TabularInline):
+    formfield_overrides = {
+        models.FileField: {"widget": AdminFileWidget},
+    }
     model = BarrierPlanFile
 
 
@@ -95,6 +100,9 @@ class BarrierPlanAdmin(
 
 
 class BarrierRealFileInline(admin.TabularInline):
+    formfield_overrides = {
+        models.FileField: {"widget": AdminFileWidget},
+    }
     model = BarrierRealFile
 
 
