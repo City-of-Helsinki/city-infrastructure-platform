93703a27b2196183f48c4e5d2c27328780edee53
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-01-21 21:06:13 +0200

    Add City Furniture app

---
+++ city_furniture/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class CityFurnitureConfig(AppConfig):
+    name = "city_furniture"
+    verbose_name = _("City furniture")
