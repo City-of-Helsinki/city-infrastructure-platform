5d0abc20296a2d2150da3c2c87422b3237888fe6
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-02-01 14:50:44 +0200

    Add admins UIs for City Furniture models

---
+++ city_furniture/admin/common.py
@@ -0,0 +1,49 @@
+from django.contrib.gis import admin
+
+from city_furniture.models.common import CityFurnitureColor, CityFurnitureDeviceType, CityFurnitureTarget
+from traffic_control.admin.audit_log import AuditLogHistoryAdmin
+from traffic_control.mixins import EnumChoiceValueDisplayAdminMixin
+
+__all__ = ("CityFurnitureDeviceTypeAdmin",)
+
+
+@admin.register(CityFurnitureDeviceType)
+class CityFurnitureDeviceTypeAdmin(EnumChoiceValueDisplayAdminMixin, AuditLogHistoryAdmin):
+    list_display = (
+        "id",
+        "code",
+        "class_type",
+        "function_type",
+        "icon",
+        "description",
+        "size",
+        "target_model",
+    )
+    ordering = ("code", "class_type", "function_type")
+    actions = None
+
+
+@admin.register(CityFurnitureColor)
+class CityFurnitureColorAdmin(AuditLogHistoryAdmin):
+    list_display = (
+        "id",
+        "name",
+        "rgb",
+    )
+    ordering = ("name",)
+    actions = None
+
+
+@admin.register(CityFurnitureTarget)
+class CityFurnitureTargetAdmin(AuditLogHistoryAdmin):
+    list_display = (
+        "id",
+        "name_fi",
+        "name_sw",
+        "name_en",
+        "description",
+        "source_id",
+        "source_name",
+    )
+    ordering = ("name_fi",)
+    actions = None
