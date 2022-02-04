df37ae2b409ff4c77b27a34ba27921b59a07ff09
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-02-04 13:14:45 +0200

    Add City Furniture Signposts CSV importer for Kantakaupungin Rantareitti

---
+++ city_furniture/management/commands/import_furniture_signpost_reals_kantakaupunki.py
@@ -0,0 +1,143 @@
+import csv
+import os
+from datetime import datetime
+from functools import lru_cache
+
+from django.conf import settings
+from django.contrib.gis.geos import Point
+from django.core.management.base import BaseCommand, CommandError
+from django.db.transaction import atomic
+
+from city_furniture.models import FurnitureSignpostReal
+from city_furniture.models.common import CityFurnitureColor, CityFurnitureDeviceType, CityFurnitureTarget
+from city_furniture.models.furniture_signpost import ArrowDirection
+from traffic_control.models import MountType
+from traffic_control.utils import get_default_owner
+from users.utils import get_system_user
+
+SOURCE_NAME = "Kantakaupungin Rantareitti 2022 csv"
+
+
+class Command(BaseCommand):
+    help = "Import Kantakaupungin Rantareitti Furniture Signpost reals from a csv file"
+    step = 1000
+    user = get_system_user()
+    owner = get_default_owner()
+    target = CityFurnitureTarget.objects.get_or_create(name_fi="Kantakaupungin Rantareitti")[0]
+    new_mount_types = {
+        "Ristikkopylväs": {
+            "code": "LATTICECOLUMN",
+            "description": "Lattice column",
+            "description_fi": "Ristikkopylväs",
+        },
+        "Katuvalopylväs": {
+            "code": "STREETLIGHTPOLE",
+            "description": "Street light pole",
+            "description_fi": "Katuvalopylväs",
+        },
+        "Tolppa": {  # Should already exist with a different description_fi value
+            "code": "POLE",
+            "description": "Pole",
+            "description_fi": "Opaste",
+        },
+    }
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "filename",
+            nargs="?",
+            type=str,
+            help="Path to the Kantakaupungin rantareitti furniture signpost reals csv file",
+            default="opastus_tietomalli.csv",
+        )
+
+    @lru_cache()
+    def get_device_type(self, signpost_type: str) -> CityFurnitureDeviceType:
+        signpost_types = {
+            "reittivahvennelaatta": "Rantareitti-1A",
+            "tarrat": "Rantareitti-2A",
+            "pollarit": "Rantareitti-3",
+            "kartat": "Rantareitti-4A",
+            "viitat": "Rantareitti-5",
+        }
+        return CityFurnitureDeviceType.objects.get(code=signpost_types[signpost_type])
+
+    @lru_cache()
+    def get_mount_type(self, mount_type: str) -> MountType:
+        """Get mount type for Furniture Signpost by description_fi, if it's a new type then it's created instead"""
+        if mount_type in self.new_mount_types:
+            return MountType.objects.get_or_create(
+                description_fi=self.new_mount_types[mount_type]["description_fi"],
+                defaults=dict(
+                    code=self.new_mount_types[mount_type]["code"],
+                    description=self.new_mount_types[mount_type]["description"],
+                ),
+            )[0]
+        return MountType.objects.get(description_fi=mount_type)
+
+    @lru_cache()
+    def get_color(self, sign_color: str) -> CityFurnitureColor:
+        return CityFurnitureColor.objects.get(name=sign_color)
+
+    def str_to_date(self, date_str: str) -> datetime.date:
+        try:
+            return datetime.strptime(date_str, "%Y-%m-%d")
+        except ValueError:
+            return datetime.strptime(date_str, "%m/%d/%y")
+
+    def import_row_signpost(self, row):
+        location = Point(float(row["latitude"]), float(row["longitude"]), 0, srid=settings.SRID)
+
+        signpost, _ = FurnitureSignpostReal.objects.update_or_create(
+            source_name=SOURCE_NAME,
+            source_id=row["id"],
+            defaults={
+                "device_type": self.get_device_type(row["device_type"]),
+                "location": location,
+                "owner": self.owner,
+                "target": self.target,
+                "location_name": row["location_name"],
+                "direction": int(row["direction"]) if row["direction"] else 0,
+                "height": int(row["height"]) if row["height"] else None,
+                "order": int(row["order"]) if row["order"] else 1,
+                "mount_type": self.get_mount_type(row["mount_type"]) if row["mount_type"] else None,
+                "arrow_direction": ArrowDirection[row["arrow_direction"]] if row["arrow_direction"] else None,
+                "color": self.get_color(row["sign_color"]) if row["sign_color"] else None,
+                "pictogram": row["pictogram"],
+                "value": int(row["value"]) if row["value"] else None,
+                "text_content_fi": row["text_content_fi"],
+                "text_content_sw": row["text_content_sw"],
+                "text_content_en": row["text_content_en"],
+                "content_responsible_entity": row["content_responsible_entity"],
+                "validity_period_start": (
+                    self.str_to_date(row["validity_period_start"]) if row["validity_period_start"] else None
+                ),
+                "validity_period_end": (
+                    self.str_to_date(row["validity_period_end"]) if row["validity_period_end"] else None
+                ),
+                "additional_material_url": row["additional_material_url"],
+                "created_by": self.user,
+                "updated_by": self.user,
+                "source_name": SOURCE_NAME,
+            },
+        )
+
+    @atomic
+    def handle(self, *args, **options):
+        filename = options["filename"]
+        if not os.path.exists(filename):
+            raise CommandError(f"File {filename} does not exist")
+
+        self.stdout.write("Importing Kantakaupungin Rantareitti Furniture Signposts...")
+        count = 0
+
+        with open(filename, encoding="utf-8-sig") as f:
+            csv_reader = csv.DictReader(f, delimiter=",")
+            for row in csv_reader:
+                self.import_row_signpost(row)
+                count += 1
+
+                if count % self.step == 0:
+                    self.stdout.write(f"{count} traffic signs are imported...")
+
+        self.stdout.write(f"{count} traffic signs are imported")
---
+++ city_furniture/tests/management/commands/test_import_furniture_signposts_kantakaupunki.py
@@ -0,0 +1,68 @@
+import os
+from unittest.mock import patch
+
+from django.core.management import call_command, CommandError
+from django.test import TestCase
+
+from city_furniture.models import FurnitureSignpostReal
+from city_furniture.models.common import CityFurnitureDeviceType
+from city_furniture.tests.factories import get_city_furniture_color
+from traffic_control.models import MountType
+from traffic_control.tests.management.commands.utils import mock_open
+
+
+class ImportTrafficControlDeviceTypesTestCase(TestCase):
+    def test_raise_command_error_if_no_file_found(self):
+        self.assertRaises(
+            CommandError,
+            call_command,
+            "import_furniture_signpost_reals_kantakaupunki",
+            "dummy.csv",
+        )
+
+    @patch("os.path.exists", return_value=True)
+    def test_import_traffic_signs_success(self, mock_exists):
+        data = os.linesep.join(
+            [
+                "id,device_type,longitude,latitude,location_name,direction,height,order,mount_type,arrow_direction,sign_color,pictogram,value,text_content_fi,text_content_sw,text_content_en,content_responsible_entity,validity_period_start,validity_period_end,additional_material_url",  # noqa: E501
+                "A42,reittivahvennelaatta,25497601.12758740,6672058.993934970,Laivasillankatu,45,140,,Tolppa,,Kantakaupunki,,,,,,,,,,",  # noqa: E501
+                "A1B,reittivahvennelaatta,25497601.12758740,6672058.993934970,JokuKatu,0,220,1,Katuvalopylväs,UP,Kantakaupunki,piktogrammi,,Suomi,Svenska,English,Kaupunki,2020-01-01,2020-12-31,https://example.com/",  # noqa: E501
+                "A2B,tarrat,25496195.72344000,6673869.586320790,JokuKatu,0,220,1,Katuvalopylväs,UP,Kantakaupunki,piktogrammi,,Suomi,Svenska,English,Kaupunki,2020-01-01,2020-12-31,https://example.com/",  # noqa: E501
+                "A3B,kartat,25497707.19834310,6672915.894190060,JokuKatu,0,220,1,Tolppa,UP,Kantakaupunki,piktogrammi,,Suomi,Svenska,English,Kaupunki,2020-01-01,2020-12-31,https://example.com/",  # noqa: E501
+                "A4B,pollarit,25496702.18210040,6671287.668982360,JokuKatu,0,220,1,Tolppa,BOTTOM_RIGHT,Kantakaupunki,piktogrammi,,Suomi,Svenska,English,Kaupunki,2020-01-01,2020-12-31,https://example.com/",  # noqa: E501
+                "A5B,viitat,25495251.56117200,6672573.32108099,JokuKatu,0,220,1,Tolppa,DOWN,Kantakaupunki,piktogrammi,500,Suomi,Svenska,English,Kaupunki,2020-01-01,2020-12-31,https://example.com/",  # noqa: E501
+                "A6B,viitat,25495251.56117200,6672573.32108099,,,,,,,,,,,,,,,,",
+            ]
+        )
+
+        with patch(
+            "city_furniture.management.commands.import_furniture_signpost_reals_kantakaupunki.open",
+            mock_open(read_data=data),
+        ):
+            # Setup device types
+            for code in [
+                "Rantareitti-1A",
+                "Rantareitti-1B",
+                "Rantareitti-2A",
+                "Rantareitti-2B",
+                "Rantareitti-3",
+                "Rantareitti-4A",
+                "Rantareitti-4B",
+                "Rantareitti-5",
+            ]:
+                CityFurnitureDeviceType.objects.create(
+                    code=code,
+                    class_type="1030",
+                    function_type="1090",
+                    target_model="furniture_signpost",
+                )
+            MountType.objects.get_or_create(code="LIGHTPOLE", description="pole", description_fi="Katuvalopylväs")
+            MountType.objects.get_or_create(code="POLE", description="pole", description_fi="Opaste")
+
+            get_city_furniture_color("Kantakaupunki")
+
+            call_command("import_furniture_signpost_reals_kantakaupunki", "test-data.csv")
+
+            self.assertEqual(FurnitureSignpostReal.objects.count(), 7)
+            fsr = FurnitureSignpostReal.objects.first()
+            self.assertEqual(fsr.source_name, "Kantakaupungin Rantareitti 2022 csv")
