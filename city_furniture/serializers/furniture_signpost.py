6beceb18ecc245529127d71dca093d7e026c9e0e
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-02-05 17:20:33 +0200

    Add Furniture Signpost REST API endpoints

---
+++ city_furniture/serializers/furniture_signpost.py
@@ -0,0 +1,97 @@
+from enumfields.drf import EnumSupportSerializerMixin
+from rest_framework import serializers
+from rest_framework_gis.fields import GeometryField
+
+from city_furniture.enums import CityFurnitureDeviceTypeTargetModel
+from city_furniture.models import (
+    FurnitureSignpostPlan,
+    FurnitureSignpostPlanFile,
+    FurnitureSignpostReal,
+    FurnitureSignpostRealFile,
+    FurnitureSignpostRealOperation,
+)
+from city_furniture.models.common import CityFurnitureDeviceType
+from traffic_control.models import OperationType
+
+
+class FurnitureSignpostPlanFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FurnitureSignpostPlanFile
+        fields = "__all__"
+
+
+class FurnitureSignpostPlanSerializer(EnumSupportSerializerMixin, serializers.ModelSerializer):
+    files = FurnitureSignpostPlanFileSerializer(many=True, read_only=True)
+    device_type = serializers.PrimaryKeyRelatedField(
+        queryset=CityFurnitureDeviceType.objects.for_target_model(CityFurnitureDeviceTypeTargetModel.FURNITURE_SIGNPOST)
+    )
+
+    class Meta:
+        model = FurnitureSignpostPlan
+        read_only_fields = (
+            "created_by",
+            "updated_by",
+            "deleted_by",
+            "deleted_at",
+        )
+        exclude = ("mount_type", "is_active", "deleted_at", "deleted_by")
+
+
+class FurnitureSignpostPlanGeoJSONSerializer(FurnitureSignpostPlanSerializer):
+    location = GeometryField()
+
+
+class FurnitureSignpostRealFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FurnitureSignpostRealFile
+        fields = "__all__"
+
+
+class FurnitureSignpostRealOperationSerializer(serializers.ModelSerializer):
+    operation_type = serializers.StringRelatedField()
+    operation_type_id = serializers.PrimaryKeyRelatedField(
+        queryset=OperationType.objects.filter(furniture_signpost=True),
+        source="operation_type",
+    )
+
+    class Meta:
+        model = FurnitureSignpostRealOperation
+        fields = ("id", "operation_type", "operation_type_id", "operation_date")
+
+    def create(self, validated_data):
+        # Inject related object to validated data
+        furniture_signpost_real = FurnitureSignpostReal.objects.get(
+            pk=self.context["view"].kwargs["furniture_signpost_real_pk"]
+        )
+        validated_data["furniture_signpost_real"] = furniture_signpost_real
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        # Inject related object to validated data
+        furniture_signpost_real = FurnitureSignpostReal.objects.get(
+            pk=self.context["view"].kwargs["furniture_signpost_real_pk"]
+        )
+        validated_data["furniture_signpost_real"] = furniture_signpost_real
+        return super().update(instance, validated_data)
+
+
+class FurnitureSignpostRealSerializer(EnumSupportSerializerMixin, serializers.ModelSerializer):
+    files = FurnitureSignpostRealFileSerializer(many=True, read_only=True)
+    device_type = serializers.PrimaryKeyRelatedField(
+        queryset=CityFurnitureDeviceType.objects.for_target_model(CityFurnitureDeviceTypeTargetModel.FURNITURE_SIGNPOST)
+    )
+    operations = FurnitureSignpostRealOperationSerializer(many=True, required=False, read_only=True)
+
+    class Meta:
+        model = FurnitureSignpostReal
+        read_only_fields = (
+            "created_by",
+            "updated_by",
+            "deleted_by",
+            "deleted_at",
+        )
+        exclude = ("mount_type", "is_active", "deleted_at", "deleted_by")
+
+
+class FurnitureSignpostRealGeoJSONSerializer(FurnitureSignpostRealSerializer):
+    location = GeometryField()
--- city_furniture/tests/factories.py
+++ city_furniture/tests/factories.py
@@ -1,5 +1,71 @@
-from city_furniture.models.common import CityFurnitureColor
+import datetime
+from typing import Optional
+
+from city_furniture.enums import CityFurnitureDeviceTypeTargetModel
+from city_furniture.models import FurnitureSignpostPlan, FurnitureSignpostReal, FurnitureSignpostRealOperation
+from city_furniture.models.common import CityFurnitureColor, CityFurnitureDeviceType
+from traffic_control.tests.factories import get_operation_type, get_owner, get_user
+from traffic_control.tests.test_base_api_3d import test_point_3d
 
 
 def get_city_furniture_color(name="Color", rgb="#FFFFFF"):
     return CityFurnitureColor.objects.get_or_create(name=name, defaults=dict(rgb=rgb))[0]
+
+
+def get_city_furniture_device_type(
+    code: str = "CODE",
+    class_type: str = "1030",
+    function_type: str = "1090",
+    target_model: Optional[CityFurnitureDeviceTypeTargetModel] = None,
+):
+    return CityFurnitureDeviceType.objects.get_or_create(
+        code=code,
+        defaults=dict(
+            class_type=class_type,
+            function_type=function_type,
+            target_model=target_model,
+        ),
+    )[0]
+
+
+def get_furniture_signpost_plan(location=test_point_3d, owner=None, device_type=None):
+    user = get_user("test_user")
+    owner = owner or get_owner()
+    device_type = device_type or get_city_furniture_device_type()
+
+    return FurnitureSignpostPlan.objects.get_or_create(
+        location=location,
+        owner=owner,
+        device_type=device_type,
+        created_by=user,
+        updated_by=user,
+    )[0]
+
+
+def get_furniture_signpost_real(location=test_point_3d, owner=None, device_type=None):
+    user = get_user("test_user")
+    owner = owner or get_owner()
+    device_type = device_type or get_city_furniture_device_type()
+
+    return FurnitureSignpostReal.objects.get_or_create(
+        location=location,
+        owner=owner,
+        device_type=device_type,
+        created_by=user,
+        updated_by=user,
+    )[0]
+
+
+def add_furniture_signpost_real_operation(furniture_signpost_real, operation_type=None, operation_date=None):
+    if not operation_type:
+        operation_type = get_operation_type()
+    if not operation_date:
+        operation_date = datetime.date.today()
+    user = get_user("test_user")
+    return FurnitureSignpostRealOperation.objects.create(
+        operation_type=operation_type,
+        operation_date=operation_date,
+        furniture_signpost_real=furniture_signpost_real,
+        created_by=user,
+        updated_by=user,
+    )
