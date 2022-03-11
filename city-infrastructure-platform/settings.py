312ee0a442b32f8f1ed7f980738806443854b415
Lauri Virtanen <lauri.virtanen@vincit.fi>
2022-03-11 12:04:50 +0200

    Update to Django 3.2 LTS
    Update urls in settings.py accordingly

--- city-infrastructure-platform/settings.py
+++ city-infrastructure-platform/settings.py
@@ -2,10 +2,10 @@
 Django settings for city-infrastructure-platform project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/topics/settings/
+https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/2.2/ref/settings/
+https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
@@ -133,8 +133,8 @@ LOGGING = {
 
 # Application definition
 DJANGO_APPS = [
-    "helusers",
     "social_django",
+    "helusers.apps.HelusersConfig",
     "helusers.apps.HelusersAdminConfig",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -212,13 +212,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "city-infrastructure-platform.wsgi.application"
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
@@ -228,7 +228,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = "fi"
 LANGUAGES = [("fi", _("Finnish")), ("en", _("English"))]
 TIME_ZONE = "Europe/Helsinki"
@@ -237,7 +237,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 var_root = env.path("VAR_ROOT")
 STATIC_ROOT = var_root("static")
 MEDIA_ROOT = var_root("media")
