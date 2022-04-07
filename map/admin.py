d0cd7bb8bf2d82f3ffa353ebec84d195a6da6d08
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-04-08 00:33:07 +0300

    Add `use_traffic_sign_icons` to Layer model

--- map/admin.py
+++ map/admin.py
@@ -5,6 +5,14 @@ from map.models import Layer
 
 @admin.register(Layer)
 class LayerAdmin(admin.ModelAdmin):
-    list_display = ("identifier", "name_fi", "name_en", "is_basemap", "order", "filter_fields")
+    list_display = (
+        "identifier",
+        "name_fi",
+        "name_en",
+        "is_basemap",
+        "order",
+        "filter_fields",
+        "use_traffic_sign_icons",
+    )
     search_fields = ("identifier", "name_fi", "name_en")
     list_filter = ("is_basemap",)
