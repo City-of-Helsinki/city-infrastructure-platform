b6b5e97016eabfe673e6755bc610a257a41a7128
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-04-14 19:05:23 +0300

    Add tree style filter for responsible entity in REST API
    When the parent's id is searched for, all objects related to any descendants are also included

--- city_furniture/filters.py
+++ city_furniture/filters.py
@@ -1,4 +1,6 @@
+from django_filters import Filter
 from django_filters.rest_framework import FilterSet
+from rest_framework.exceptions import NotFound
 
 from city_furniture.models import FurnitureSignpostPlan, FurnitureSignpostReal, FurnitureSignpostRealOperation
 from city_furniture.models.common import (
@@ -10,8 +12,24 @@ from city_furniture.models.common import (
 from traffic_control.filters import GenericMeta, OperationalAreaFilter
 
 
+class ResponsibleEntityFilter(Filter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def filter(self, qs, value):
+        if value:
+            selected_object = ResponsibleEntity.objects.filter(id=value).first()
+            if selected_object is None:
+                raise NotFound({"responsible_entity": f"Responsible Entity with ID `{value}` was not found."})
+
+            descendant_ids = selected_object.get_descendants(include_self=True).values_list("id", flat=True).distinct()
+            qs = qs.filter(responsible_entity__id__in=descendant_ids)
+        return qs
+
+
 class FurnitureSignpostPlanFilterSet(FilterSet):
     operational_area = OperationalAreaFilter()
+    responsible_entity = ResponsibleEntityFilter()
 
     class Meta(GenericMeta):
         model = FurnitureSignpostPlan
@@ -19,6 +37,7 @@ class FurnitureSignpostPlanFilterSet(FilterSet):
 
 class FurnitureSignpostRealFilterSet(FilterSet):
     operational_area = OperationalAreaFilter()
+    responsible_entity = ResponsibleEntityFilter()
 
     class Meta(GenericMeta):
         model = FurnitureSignpostReal
