7c51665f10a700211c757e51bb287cee358aac27
Eemeli Ranta <eemeli.ranta@vincit.fi>
2022-04-20 18:30:43 +0300

    Use ResponsibleEntity Permissions in FurnitureSignpost admin
    - Return only devices which user is responsible for in Admin
    - Restrict Responsible Entity field choices in admin to ones user is a part of
    - Prevent user from creating new devices if they don't have any Responsible Entities set or can bypass permission

--- city_furniture/forms.py
+++ city_furniture/forms.py
@@ -1,14 +1,15 @@
+from city_furniture.admin.utils import ResponsibleEntityPermissionAdminFormMixin
 from city_furniture.models import FurnitureSignpostPlan, FurnitureSignpostReal
 from traffic_control.forms import Point3DFieldForm
 
 
-class FurnitureSignpostRealModelForm(Point3DFieldForm):
+class FurnitureSignpostRealModelForm(ResponsibleEntityPermissionAdminFormMixin, Point3DFieldForm):
     class Meta:
         model = FurnitureSignpostReal
         fields = "__all__"
 
 
-class FurnitureSignpostPlanModelForm(Point3DFieldForm):
+class FurnitureSignpostPlanModelForm(ResponsibleEntityPermissionAdminFormMixin, Point3DFieldForm):
     class Meta:
         model = FurnitureSignpostPlan
         fields = "__all__"
