f066cc9a853cdabfe499038fe30c1857d9ec40e6
Lauri Virtanen <lauri.virtanen@vincit.fi>
2022-04-26 14:17:48 +0300

    Fix feature info mock feature to match current type definition

--- map-view/src/components/FeatureInfo.test.tsx
+++ map-view/src/components/FeatureInfo.test.tsx
@@ -9,7 +9,7 @@ test("renders feature info", () => {
       geometry: { type: "Point", coordinates: [1, 1] },
       geometry_name: "location",
       id_: "traffic_sign_real.20950c38-eb7e-11ea-adc1-0242ac120002",
-      values_: {
+      getProperties: () => ({
         id: "fcdbbc6f-5903-4574-baa3-fef33a4b0621",
         // @ts-ignore
         geometry: { flatCoordinates: [25496040, 6676200, 0] },
@@ -19,7 +19,7 @@ test("renders feature info", () => {
         device_type_code: "ABC",
         device_type_description: "Sample description",
         device_plan_id: "ABC-123",
-      },
+      }),
       type: "Feature",
     },
   ];
