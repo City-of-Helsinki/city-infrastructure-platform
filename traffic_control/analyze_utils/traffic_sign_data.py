a7ad91fd5593ab7dd52ecf22a94d5288343b0671
Janne Forsman <janne.forsman@vincit.fi>
2024-06-27 15:07:58 +0300

    streetview data import testing

--- traffic_control/analyze_utils/traffic_sign_data.py
+++ traffic_control/analyze_utils/traffic_sign_data.py
@@ -1,6 +1,17 @@
 import csv
+from datetime import datetime
+from decimal import Decimal
 
 from django.contrib.gis.geos import Point
+
+from django.conf import settings
+
+from traffic_control.models.additional_sign import AdditionalSignReal
+from traffic_control.models.mount import MountType, MountReal
+from traffic_control.models.common import Owner
+from traffic_control.models.traffic_sign import TrafficSignReal
+from traffic_control.models.signpost import SignpostReal
+from traffic_control.models.common import TrafficControlDeviceType
 
 
 class TrafficSignAnalyzer:
@@ -216,5 +227,206 @@ class TrafficSignAnalyzer:
                        )}
 
 
+class TrafficSignImporter:
+    SOURCE_NAME = "StreetScan"
+
+    def __init__(self, mount_data, sign_data, additional_sign_data):
+        self.mount_data = mount_data
+        self.sign_data = sign_data
+        self.additional_sign_data = additional_sign_data
+        self.mount_types_by_name = self._get_mount_types_by_name()
+        self.default_owner = self._get_default_owner()
+        self.device_types_by_code = self._get_device_types_by_code()
+
+        self.mount_reals_by_source_id = None
+        self.sign_reals_by_source_id = None
+
+        self.errors = []
+
+    def import_data(self):
+        self._import_mount_reals()
+        self._import_sign_data()
+        self._import_signpost_data()
+        self._import_additional_sign_data()
+
+        return self.errors
+
+    def _import_mount_reals(self):
+        MountReal.objects.bulk_create(self._get_mount_objects(), batch_size=1000, ignore_conflicts=True)
+        self.mount_reals_by_source_id = self._get_mount_reals_by_source_id()
+
+    def _get_mount_objects(self):
+        for mount_id, mount_data in self.mount_data.items():
+            yield MountReal(
+                mount_type=self.mount_types_by_name[mount_data["tyyppi"]],
+                source_id=mount_id,
+                source_name=self.SOURCE_NAME,
+                location=Point(float(mount_data["x"]), float(mount_data["y"]), float(mount_data["z"]), srid=settings.SRID),
+                owner=self.default_owner,
+            )
+
+    def _get_mount_reals_by_source_id(self):
+        return {x.source_id: x.id for x in MountReal.objects.filter(source_name=self.SOURCE_NAME)}
+
+    def _get_sign_reals_by_source_id(self):
+        return {x.source_id: x.id for x in TrafficSignReal.objects.filter(source_name=self.SOURCE_NAME)}
+
+    def _import_sign_data(self):
+        TrafficSignReal.objects.bulk_create(
+            self._get_sign_objects(),
+            batch_size=1000,
+            ignore_conflicts=True
+        )
+        self.sign_reals_by_source_id = self._get_sign_reals_by_source_id()
+
+    def _get_sign_objects(self):
+        for sign_id, sign_data in self.sign_data.items():
+            if not is_signpost(sign_data) and not should_be_ignored_totally(sign_data):
+                yield TrafficSignReal(
+                    owner=self.default_owner,
+                    device_type=self._get_sign_device_type(sign_data["merkkikoodi"]),
+                    scanned_at=self._get_sign_scanned_at(sign_data.get("tallennusajankohta")),
+                    value=self._get_sign_value(sign_data.get("numerokoodi")),
+                    source_id=sign_id,
+                    source_name=self.SOURCE_NAME,
+                    location=Point(float(sign_data["x"]), float(sign_data["y"]), float(sign_data["z"]), srid=settings.SRID),
+                    direction=sign_data.get("atsimuutti"),
+                    height=self._get_sign_height(sign_data.get("korkeus")),
+                    condition=sign_data.get("merkin_ehto"),
+                    mount_real_id=self._get_mount_real_id(sign_data["kiinnityskohta_id"]),
+                    mount_type=self.mount_types_by_name[sign_data["kiinnitys"]],
+                    txt=sign_data["teksti"]
+                )
+            else:
+                continue
+
+    def _import_signpost_data(self):
+        SignpostReal.objects.bulk_create(
+            self._get_signpost_objects(),
+            batch_size=1000,
+            ignore_conflicts=True
+        )
+
+    # TODO get rid of copy paste
+    def _get_signpost_objects(self):
+        for sign_id, sign_data in self.sign_data.items():
+            if is_signpost(sign_data) and not should_be_ignored_totally(sign_data):
+                yield SignpostReal(
+                    owner=self.default_owner,
+                    device_type=self._get_sign_device_type(sign_data["merkkikoodi"]),
+                    value=self._get_sign_value(sign_data.get("numerokoodi")),
+                    source_id=sign_id,
+                    source_name=self.SOURCE_NAME,
+                    location=Point(float(sign_data["x"]), float(sign_data["y"]), float(sign_data["z"]),
+                                   srid=settings.SRID),
+                    direction=sign_data.get("atsimuutti"),
+                    height=self._get_sign_height(sign_data.get("korkeus")),
+                    condition=sign_data.get("merkin_ehto"),
+                    mount_real_id=self._get_mount_real_id(sign_data["kiinnityskohta_id"]),
+                    mount_type=self.mount_types_by_name[sign_data["kiinnitys"]],
+                    txt=sign_data["teksti"]
+                )
+            else:
+                continue
+
+    def _import_additional_sign_data(self):
+        AdditionalSignReal.objects.bulk_create(
+            self._get_additional_sign_objects(),
+            batch_size=1000,
+            ignore_conflicts=True
+        )
+
+    def _get_additional_sign_objects(self):
+        for sign_id, sign_data in self.additional_sign_data.items():
+            if additional_sign_should_be_imported(sign_data):
+                yield AdditionalSignReal(
+                    owner=self.default_owner,
+                    device_type=self._get_sign_device_type(sign_data["merkkikoodi"]),
+                    scanned_at=self._get_sign_scanned_at(sign_data.get("tallennusajankohta")),
+                    source_id=sign_id,
+                    source_name=self.SOURCE_NAME,
+                    location=Point(float(sign_data["x"]), float(sign_data["y"]), float(sign_data["z"]),
+                                   srid=settings.SRID),
+                    direction=sign_data.get("atsimuutti"),
+                    height=self._get_sign_height(sign_data.get("korkeus")),
+                    condition=sign_data.get("merkin_ehto"),
+                    mount_real_id=self._get_mount_real_id(sign_data["kiinnityskohta_id"]),
+                    mount_type=self.mount_types_by_name[sign_data["kiinnitys"]],
+                    parent_id=self._get_sign_real_id(sign_data["lisäkilven_päämerkin_id"])
+                )
+        return None
+
+    def _get_mount_real_id(self, source_id):
+        db_id = self.mount_reals_by_source_id.get(source_id, None)
+
+        if db_id is None:
+            self.errors.append(f"mount_real with source_id: {source_id} not found")
+
+        return db_id
+
+    def _get_sign_real_id(self, source_id):
+        db_id = self.sign_reals_by_source_id.get(source_id, None)
+
+        if db_id is None:
+            self.errors.append(f"parent sign with source_id: {source_id} not found")
+
+        return db_id
+
+    @staticmethod
+    def _get_sign_scanned_at(date_str):
+        """Need to add 00 to the end as source date has only +00 as tz marker"""
+        return datetime.strptime(date_str + "00", "%Y/%m/%d %H:%M:%S%z")
+
+    @staticmethod
+    def _get_sign_value(value):
+        use_value = value.strip()
+        if use_value == "unreadable":
+            use_value = None
+        if use_value:
+            use_value = use_value[0]
+        # assume that value can be like "30" or "3,3 m", so that actual value is always the first
+        return Decimal(use_value) if use_value else None
+
+    def _get_sign_device_type(self, code):
+        dcode = self.device_types_by_code.get(code, None)
+        if not dcode:
+            self.errors.append(f"dtype code not found: {code}")
+        return dcode
+
+    @staticmethod
+    def _get_sign_height(height):
+        return int(float(height)*100)
+
+    @staticmethod
+    def _get_mount_types_by_name():
+        mount_types = list(MountType.objects.all())
+        by_fi = {mt.description_fi: mt for mt in mount_types}
+        by_en = {mt.description: mt for mt in mount_types}
+        by_fi.update(by_en)
+        return by_fi
+
+    @staticmethod
+    def _get_default_owner():
+        return Owner.objects.get(name_fi="Helsingin kaupunki")
+
+    @staticmethod
+    def _get_device_types_by_code():
+        return {dt.code: dt for dt in list(TrafficControlDeviceType.objects.all())}
+
+
 def is_additional_sign(row):
     return row["merkkikoodi"][0] in ["H", "8"]
+
+
+def is_signpost(row):
+    return row["merkkikoodi"][0] in ["6", "F", "G"]
+
+
+def should_be_ignored_totally(row):
+    return row["merkkikoodi"] in ["x", "not classified"]
+
+
+def additional_sign_should_be_imported(row):
+    if not row["lisäkilven_päämerkin_id"].strip():
+        return row["merkkikoodi"] in ["H20.91", "H20.92", "H20.93"]
+    return True
---
+++ traffic_control/management/commands/import_traffic_sign_data.py
@@ -0,0 +1,37 @@
+import pprint
+from django.core.management.base import BaseCommand
+
+from traffic_control.analyze_utils.traffic_sign_data import TrafficSignAnalyzer, TrafficSignImporter
+
+
+class Command(BaseCommand):
+    help = "Analyzes sign input data"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-mf",
+            "--mount-file",
+            type=str,
+            help="Path to the mount file in csv format",
+        )
+        parser.add_argument(
+            "-sf",
+            "--sign-file",
+            type=str,
+            help="Path to the sign file in csv format",
+        )
+
+    def handle(self, *args, **options):
+        mount_file = options["mount_file"]
+        if mount_file is None:
+            self.stderr.write(self.style.ERROR("Missing mount file"))
+            return None
+        sign_file = options["sign_file"]
+        if sign_file is None:
+            self.stderr.write(self.style.ERROR("Missing sign file"))
+            return None
+
+        analyzer = TrafficSignAnalyzer(mount_file, sign_file)
+        importer = TrafficSignImporter(analyzer.mounts_by_id, analyzer.signs_by_id, analyzer.additional_signs_by_id)
+        errors = importer.import_data()
+        pprint.pprint(set(errors))
