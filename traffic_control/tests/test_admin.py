e4ed3d5be2c0b3a5eaa086cbe742803c9cc2889f
Janne Forsman <janne.forsman@vincit.fi>
2024-10-17 16:11:12 +0300

    chore: Add more password validations * added new library django-advanced-password-validation * one number, uppercase, lowercase and special character are now required * django's built-in NumericPasswordValidator removed as it is now obsolete
    Refs: LIIK-647

--- traffic_control/tests/test_admin.py
+++ traffic_control/tests/test_admin.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
+import pytest
 from auditlog.models import LogEntry
 from django.conf import settings
 from django.contrib.admin import AdminSite
 from django.contrib.gis.geos import Point
 from django.test import RequestFactory, TestCase
+from django.urls import reverse
 
 from traffic_control.admin import BarrierRealAdmin, TrafficSignRealAdmin
 from traffic_control.enums import Lifecycle
@@ -125,3 +127,65 @@ class SoftDeleteAdminTestCase(TestCase):
         self.assertEqual(update_entries.count(), 1)
         update_entry = create_entries[0]
         self.assertEqual(update_entry.actor, None)
+
+
+TOO_CLOSE_TO_USERNAME = "Salasana on liian lähellä kohdetta käyttäjätunnus."
+TOO_SHORT = "Tämä salasana on liian lyhyt. Sen tulee sisältää ainakin 12 merkkiä."
+TOO_COMMON = "Tämä salasana on liian yleinen."
+NUMBER_MISSING = "Password must contain at least 1 number."
+UPPERCASE_MISSING = "Password must contain at least 1 uppercase character."
+LOWERCASE_MISSING = "Password must contain at least 1 lowercase character."
+SPECIAL_CHAR_MISSING = "Password must contain at least 1 special character ( !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~)."
+
+
+@pytest.mark.parametrize(
+    "username,password,expected_msgs",
+    (
+        (
+            "test",
+            "test",
+            [TOO_CLOSE_TO_USERNAME, TOO_SHORT, TOO_COMMON, NUMBER_MISSING, UPPERCASE_MISSING, SPECIAL_CHAR_MISSING],
+        ),
+        (
+            "test",
+            "TEST",
+            [TOO_CLOSE_TO_USERNAME, TOO_SHORT, TOO_COMMON, NUMBER_MISSING, LOWERCASE_MISSING, SPECIAL_CHAR_MISSING],
+        ),
+        (
+            "test",
+            "Kall3Kall3!",
+            [TOO_SHORT],
+        ),
+        (
+            "test",
+            "Kall3Kall312",
+            [SPECIAL_CHAR_MISSING],
+        ),
+        (
+            "test",
+            "kall3kall31!",
+            [UPPERCASE_MISSING],
+        ),
+        (
+            "test",
+            "KALL3KALL31!",
+            [LOWERCASE_MISSING],
+        ),
+        (
+            "test",
+            "Kall3Kall3!12",
+            [],
+        ),
+    ),
+)
+@pytest.mark.django_db
+def test__create_user_password_validation(client, admin_user, username, password, expected_msgs):
+    client.force_login(admin_user)
+    response = client.post(
+        reverse("admin:users_user_add"), {"username": username, "password": password, "password2": password}
+    )
+
+    assert response.status_code == 200
+
+    for msg in expected_msgs:
+        assert msg in response.context.get("errors")[1]
