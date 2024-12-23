07fc4aa41349b1ebe180267aadf109758e5aeb0a
Janne Forsman <janne.forsman@vincit.fi>
2024-12-23 12:47:12 +0200

    feat: Register User model to auditlog
    Refs: LIIK-709

--- users/tests/test_user_model.py
+++ users/tests/test_user_model.py
@@ -1,4 +1,5 @@
 import pytest
+from auditlog.models import LogEntry
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.contrib.gis.geos import MultiPolygon, Point, Polygon
@@ -26,11 +27,13 @@ point_outside_area = Point(MIN_X + 51.0, MIN_Y + 51.0, 0.0, srid=settings.SRID)
 @pytest.mark.django_db
 def test__user_operational_area__contains_location(location, expected):
     user = get_user()
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.CREATE).count() == 1
+
     oa = get_operational_area(area=area)
     user.operational_areas.add(oa)
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.UPDATE).count() == 1
 
     in_area = user.location_is_in_operational_area(location)
-
     assert in_area == expected
 
 
@@ -38,11 +41,13 @@ def test__user_operational_area__contains_location(location, expected):
 @pytest.mark.django_db
 def test__superuser_operational_area(location):
     user = get_user(admin=True)
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.CREATE).count() == 1
+
     oa = get_operational_area(area=area)
     user.operational_areas.add(oa)
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.UPDATE).count() == 1
 
     in_area = user.location_is_in_operational_area(location)
-
     assert in_area
 
 
@@ -50,13 +55,16 @@ def test__superuser_operational_area(location):
 @pytest.mark.django_db
 def test__user_operational_area__bypass_operational_area(location):
     user = get_user(admin=True)
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.CREATE).count() == 1
+
     user.bypass_operational_area = True
     user.save(update_fields=["bypass_operational_area"])
     oa = get_operational_area(area=area)
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.UPDATE).count() == 1
     user.operational_areas.add(oa)
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.UPDATE).count() == 2
 
     in_area = user.location_is_in_operational_area(location)
-
     assert in_area
 
 
@@ -64,12 +72,15 @@ def test__user_operational_area__bypass_operational_area(location):
 @pytest.mark.django_db
 def test__user_group_operational_area__contains_location(location, expected):
     user = get_user()
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.CREATE).count() == 1
+
     group = Group.objects.create(name="test group")
     user.groups.add(group)
+    assert LogEntry.objects.get_for_object(user).filter(action=LogEntry.Action.UPDATE).count() == 1
+
     oa = get_operational_area(area)
     group_oa = GroupOperationalArea.objects.create(group=group)
     group_oa.areas.add(oa)
 
     in_area = user.location_is_in_operational_area(location)
-
     assert in_area == expected
