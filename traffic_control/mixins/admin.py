6a02a54594473a33e9585a958b8dca4befe8bed2
Janne Forsman <janne.forsman@vincit.fi>
2025-04-28 12:56:51 +0300

    feat: update django-admin-confirmation * https://github.com/TrangPham/django-admin-confirm/issues/32 is now released so our own modifications can be removed.
    Refs: LIIK-747

--- traffic_control/mixins/admin.py
+++ traffic_control/mixins/admin.py
@@ -1,10 +1,5 @@
-from typing import Dict
-
-from admin_confirm import AdminConfirmMixin
 from django.contrib.admin import SimpleListFilter
 from django.contrib.gis.forms import OSMWidget
-from django.db.models import FileField, ImageField, ManyToManyField, Model
-from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 
 from traffic_control.models.plan import Plan
@@ -145,82 +140,3 @@ class UpdatePlanLocationAdminMixin:
 class DeviceTypeSearchAdminMixin:
     def get_search_fields(self, request):
         return self.search_fields + ("device_type__code", "device_type__legacy_code")
-
-
-class CityInfraAdminConfirmMixin(AdminConfirmMixin):
-    """Class for tweaking admin confirmation behaviour.
-    Overwrite private _get_changed_data function to support fields only in admin form not in model itself.
-    """
-
-    only_form_fields = []
-
-    def _get_changed_data(self, form: ModelForm, model: Model, obj: object, add: bool) -> Dict:
-        """
-        Given a form, detect the changes on the form from the default values (if add) or
-        from the database values of the object (model instance)
-
-        form - Submitted form that is attempting to alter the obj
-        model - the model class of the obj
-        obj - instance of model which is being altered
-        add - are we attempting to add the obj or does it already exist in the database
-
-        Returns a dictionary of the fields and their changed values if any
-        """
-
-        changed_data = {}
-        if add:
-            self._handle_add(form, model, changed_data)
-        else:
-            self._handle_change(form, model, changed_data, obj)
-
-        return changed_data
-
-    def _filtered_form_cleaned_data_items(self, form):
-        return filter(lambda x: x[0] not in self.only_form_fields, form.cleaned_data.items())
-
-    @staticmethod
-    def _display_for_changed_data(field, initial_value, new_value):
-        if not (isinstance(field, FileField) or isinstance(field, ImageField)):
-            return [initial_value, new_value]
-
-        if initial_value:
-            if new_value is False:
-                # Clear has been selected
-                return [initial_value.name, None]
-            elif new_value:
-                return [initial_value.name, new_value.name]
-            else:
-                # No cover: Technically doesn't get called in current code because
-                # This function is only called if there was a difference in the data
-                return [initial_value.name, initial_value.name]  # pragma: no cover
-
-        if new_value:
-            return [None, new_value.name]
-
-        return [None, None]
-
-    def _handle_add(self, form, model, changed_data):
-        for name, new_value in self._filtered_form_cleaned_data_items(form):
-            # Don't consider default values as changed for adding
-            field_object = model._meta.get_field(name)
-            default_value = field_object.get_default()
-            if new_value is not None and new_value != default_value:
-                # Show what the default value is
-                changed_data[name] = self._display_for_changed_data(field_object, default_value, new_value)
-
-    def _handle_change(self, form, model, changed_data, obj):
-        for name, new_value in self._filtered_form_cleaned_data_items(form):
-            # Ignore fields that are only in admin for and not in the model itself
-            # Since the form considers initial as the value first shown in the form
-            # It could be incorrect when user hits save, and then hits "No, go back to edit"
-            obj.refresh_from_db()
-
-            field_object = model._meta.get_field(name)
-            initial_value = getattr(obj, name)
-
-            # Note: getattr does not work on ManyToManyFields
-            if isinstance(field_object, ManyToManyField):
-                initial_value = field_object.value_from_object(obj)
-
-            if initial_value != new_value:
-                changed_data[name] = self._display_for_changed_data(field_object, initial_value, new_value)
