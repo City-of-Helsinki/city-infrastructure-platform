01e9340e5cd35073c9c1832835e9afe575ae7f6d
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-08-20 12:30:01 +0300

    feat: Add icon_file field to CityFurnitureDeviceType, override form widget
    Refs: LIIK-806

--- city_furniture/forms.py
+++ city_furniture/forms.py
@@ -1,7 +1,30 @@
-from city_furniture.models import FurnitureSignpostPlan, FurnitureSignpostReal
+from django import forms
+
+from city_furniture.models import CityFurnitureDeviceType, FurnitureSignpostPlan, FurnitureSignpostReal
 from traffic_control.admin.utils import ResponsibleEntityPermissionAdminFormMixin
 from traffic_control.forms import Geometry3DFieldForm, SRIDBoundGeometryFormMixin
 
+
+class AdminCityFurnitureDeviceTypeIconSelectWidget(forms.Select):
+    """
+    TODO
+    """
+
+    template_name = "admin/traffic_control/widgets/city_furniture_device_type_icon_select.html"
+
+    class Media:
+        css = {"all": ()}
+        js = ()
+
+
+
+class CityFurnitureDeviceTypeForm(forms.ModelForm):
+    class Meta:
+        model = CityFurnitureDeviceType
+        widgets = {
+            "icon_file": AdminCityFurnitureDeviceTypeIconSelectWidget,
+        }
+        fields = "__all__"
 
 class FurnitureSignpostRealModelForm(
     SRIDBoundGeometryFormMixin, ResponsibleEntityPermissionAdminFormMixin, Geometry3DFieldForm
