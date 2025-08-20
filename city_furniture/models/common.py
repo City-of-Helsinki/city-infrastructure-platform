0df546e7a1864486349d067bada4799cf2742bc8
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-08-20 12:30:01 +0300

    feat: Add icon_file field to CityFurnitureDeviceType, override form widget
    Refs: LIIK-806

--- city_furniture/models/common.py
+++ city_furniture/models/common.py
@@ -10,12 +10,21 @@ from django.utils.translation import gettext_lazy as _
 from enumfields import EnumField, EnumIntegerField
 
 from city_furniture.enums import CityFurnitureClassType, CityFurnitureDeviceTypeTargetModel, CityFurnitureFunctionType
-from traffic_control.mixins.models import SourceControlModel
+from traffic_control.mixins.models import AbstractFileModel, SourceControlModel
 
 
 class CityFurnitureDeviceTypeQuerySet(models.QuerySet):
     def for_target_model(self, target_model: CityFurnitureDeviceTypeTargetModel):
         return self.filter(Q(target_model=None) | Q(target_model=target_model))
+
+
+class CityFurnitureDeviceTypeIcon(AbstractFileModel):
+    file = models.FileField(_("File"), blank=False, null=False, upload_to="icons/city_furniture_device_type/svg/")
+
+    class Meta:
+        db_table = "city_furniture_device_type_icon"
+        verbose_name = _("City Furniture Device Type Icon")
+        verbose_name_plural = _("City Furniture Device Type Icons")
 
 
 class CityFurnitureDeviceType(models.Model):
@@ -45,6 +54,13 @@ class CityFurnitureDeviceType(models.Model):
         _("Icon"),
         max_length=100,
         blank=True,
+        help_text=_("Icon of the actual device"),
+    )
+    icon_file = models.ForeignKey(
+        CityFurnitureDeviceTypeIcon,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
         help_text=_("Icon of the actual device"),
     )
     description_fi = models.CharField(
