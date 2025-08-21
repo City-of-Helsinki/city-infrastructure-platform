d7d6b6c76c16827c45878ca483a1d381d747a288
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-08-21 15:24:04 +0300

    feat: Improve dropdown selector for the icon_file field
    We can use most of what was available for TrafficSign{Plan,Real}, except
    we're dealing with a different field name (and slightly different model
    structure), but template, javascript and CSS cane reused.
    
    Refs: LIIK-806

--- city_furniture/forms.py
+++ city_furniture/forms.py
@@ -1,21 +1,48 @@
+
 from django import forms
+from django.forms.models import ModelChoiceIteratorValue
 
 from city_furniture.models import CityFurnitureDeviceType, FurnitureSignpostPlan, FurnitureSignpostReal
+from city_furniture.models.common import CityFurnitureDeviceTypeIcon
 from traffic_control.admin.utils import ResponsibleEntityPermissionAdminFormMixin
 from traffic_control.forms import Geometry3DFieldForm, SRIDBoundGeometryFormMixin
 
 
 class AdminCityFurnitureDeviceTypeIconSelectWidget(forms.Select):
     """
-    TODO
+    Widget that allows a site administrator to choose a city furniture device type
+    and see a preview of the corresponding icon
     """
 
-    template_name = "admin/traffic_control/widgets/city_furniture_device_type_icon_select.html"
+    template_name = "admin/traffic_control/widgets/traffic_sign_icon_select.html"
 
     class Media:
-        css = {"all": ()}
-        js = ()
+        css = {"all": ("traffic_control/css/traffic_sign_icon_select.css",)}
+        js = ("traffic_control/js/traffic_sign_icon_select.js",)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.icon_url_mapping = None
+
+    def get_icon_url(self, value):
+        if not self.icon_url_mapping:
+            self.icon_url_mapping = {}
+            icons = CityFurnitureDeviceTypeIcon.objects.all().only("id", "file")
+            for icon in icons:
+                self.icon_url_mapping[icon.id] = icon.file.url
+        return self.icon_url_mapping.get(value, "")
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context["icon_path"] = self.get_icon_url(value)
+        return context
+
+    def create_option(self, name, value, *args, **kwargs):
+        if isinstance(value, ModelChoiceIteratorValue):
+            value = value.value
+        option = super().create_option(name, value, *args, **kwargs)
+        option["attrs"]["icon-url"] = self.get_icon_url(value)
+        return option
 
 
 class CityFurnitureDeviceTypeForm(forms.ModelForm):
