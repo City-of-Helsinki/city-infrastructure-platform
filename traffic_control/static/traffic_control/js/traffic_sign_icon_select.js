cfaaa11a877e3880aa79e090665c87765b079025
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-08-27 09:46:51 +0300

    fix: UI crash condition on the dropdown selector with icon preview
    The UI would crash when the user would add a new object using the inline
    + on the dropdown selector with icon preview. Now it simply quietly
    chooses not to show the icon when is not yet loaded.
    
    Refs: LIIK-806

--- traffic_control/static/traffic_control/js/traffic_sign_icon_select.js
+++ traffic_control/static/traffic_control/js/traffic_sign_icon_select.js
@@ -5,7 +5,13 @@
  * @param iconUrl The new icon url
  */
 function updateTrafficSignIcon(icon, iconUrl) {
-  icon.src = iconUrl;
-  icon.alt = iconUrl.split("/").pop();
-  icon.style.display = iconUrl ? "block" : "none";
+  if (typeof iconUrl !== "string") { // Happens when a new item is introduced to the dropdown after load
+    icon.src = null;
+    icon.alt = null;
+    icon.style.display = "none";
+  } else {
+    icon.src = iconUrl;
+    icon.alt = iconUrl.split("/").pop();
+    icon.style.display = iconUrl ? "block" : "none";
+  }
 }
