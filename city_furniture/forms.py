de728de1fabee843e6ad18f9c402f64ca572f010
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-08-28 16:35:46 +0300

    feat: Scan incoming icon files for viruses
    Refs: LIIK-806

--- city_furniture/forms.py
+++ city_furniture/forms.py
@@ -1,11 +1,15 @@
 
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms.models import ModelChoiceIteratorValue
+from django.utils.translation import gettext_lazy as _
 
 from city_furniture.models import CityFurnitureDeviceType, FurnitureSignpostPlan, FurnitureSignpostReal
 from city_furniture.models.common import CityFurnitureDeviceTypeIcon
 from traffic_control.admin.utils import ResponsibleEntityPermissionAdminFormMixin
 from traffic_control.forms import Geometry3DFieldForm, SRIDBoundGeometryFormMixin
+from traffic_control.services.virus_scan import add_virus_scan_errors_to_auditlog, get_error_details_message
+from traffic_control.utils import get_icon_upload_obstacles
 
 
 class AdminCityFurnitureDeviceTypeIconSelectWidget(forms.Select):
@@ -43,6 +47,28 @@ class AdminCityFurnitureDeviceTypeIconSelectWidget(forms.Select):
         option = super().create_option(name, value, *args, **kwargs)
         option["attrs"]["icon-url"] = self.get_icon_url(value)
         return option
+
+
+class CityFurnitureDeviceTypeIconForm(forms.ModelForm):
+    class Meta:
+        model = CityFurnitureDeviceTypeIcon
+        fields = "__all__"
+
+    def clean(self):
+        cleaned_data = super().clean()
+        file = cleaned_data.get('file')
+
+        if file:
+            illegal_file_types, virus_scan_errors = get_icon_upload_obstacles([file])
+
+            if illegal_file_types:
+                raise ValidationError(_(f"Illegal file types: {', '.join(illegal_file_types)}"))
+
+            if virus_scan_errors:
+                add_virus_scan_errors_to_auditlog(virus_scan_errors, None, CityFurnitureDeviceTypeIcon, None)
+                raise ValidationError(_(f"Virus scan failure: {get_error_details_message(virus_scan_errors)}"))
+
+        return cleaned_data
 
 
 class CityFurnitureDeviceTypeForm(forms.ModelForm):
