145305b62c9db4c3843b8a62b53730cc90da5e2b
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-09-04 14:51:27 +0300

    Enable usage of azurite for locally testing blobstorage operations
    Other changes:
    
    * Remove deprecated version line on docker-compose.yml
    
    Refs: LIIK-806

--- cityinfra/settings.py
+++ cityinfra/settings.py
@@ -72,6 +72,7 @@ env = environ.Env(
     CSRF_COOKIE_SAMESITE=(str, "Strict"),
     SESSION_COOKIE_SAMESITE=(str, "Lax"),
     CITYINFRA_MAXIMUM_RESULTS_PER_PAGE=(int, 10000),
+    EMULATE_AZURE_BLOBSTORAGE=(bool, False),
 )
 
 if os.path.exists(env_file):
@@ -350,6 +351,20 @@ if not DEBUG:
 if DEBUG:
     CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGIN_REGEXES = env("CORS_ALLOWED_ORIGIN_REGEXES")
+
+# Azurite-specific configuration, meant only for local testing
+EMULATE_AZURE_BLOBSTORAGE = env.bool("EMULATE_AZURE_BLOBSTORAGE")
+if EMULATE_AZURE_BLOBSTORAGE:
+    print("Configuring default storage for azurite...")
+    STORAGES["default"] = {
+        "BACKEND": "storages.backends.azure_storage.AzureStorage",
+        "OPTIONS": {
+            # NOTE (2025-09-11 thiago): This is public info
+            # https://github.com/Azure/Azurite/blob/92743bac3cf580c6dfe1ecc9ac777a6ce16cd985/README.md#connection-strings
+            "connection_string": "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;",
+            "azure_container": "media",
+        },
+    }
 
 # OpenShift-specific configuration
 OPENSHIFT_DEPLOYMENT = env.bool("OPENSHIFT_DEPLOYMENT")
