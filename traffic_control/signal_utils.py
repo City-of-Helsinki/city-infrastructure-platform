ac803218e1c3c6dca812903ccd1f9a3a81ba1c79
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-09-10 18:05:10 +0300

    Share implemntation details of PNG generation signal handlers
    Refs: LIIK-806

---
+++ traffic_control/signal_utils.py
@@ -0,0 +1,41 @@
+import logging
+import os
+
+import cairosvg
+from django.conf import settings
+from django.core.files.base import ContentFile
+
+logger = logging.getLogger("django")
+
+
+def generate_pngs_on_svg_save(*, instance, png_folder):
+    """
+    Shared implementation of icon PNG file generation.
+    """
+    # This block ensures the signal only runs for objects that have an SVG file.
+    if instance.file and instance.file.name.endswith(".svg"):
+        try:
+            with instance.file.open("rb") as svg_file:
+                svg_bytestring = svg_file.read()
+        except IOError as e:
+            logger.error(f"Unable to read {instance.file.name}: {e}")
+            return
+
+        for size in settings.PNG_ICON_SIZES:
+            svg_file_name = os.path.basename(instance.file.name)
+            png_file_name = svg_file_name.replace(".svg", ".png")
+            png_file_path = os.path.join(png_folder, str(size), png_file_name)
+
+            try:
+                png_data = cairosvg.svg2png(bytestring=svg_bytestring, output_width=size, output_height=size)
+            except Exception as e:
+                logger.error(f"Unable to convert {png_file_name} to PNG: {e}")
+                return
+            try:
+                png_file_content = ContentFile(png_data)
+                instance.file.storage.save(png_file_path, png_file_content)
+            except Exception as e:
+                logger.error(f"Unable to store {png_file_path}: {e}")
+                return
+
+            logger.info(f"PNG icon generated: {png_file_path}")
