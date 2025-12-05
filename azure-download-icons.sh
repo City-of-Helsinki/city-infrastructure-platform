6e6965d6b9e16d2a5c4bdca2c0183fe8b8d7543d
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-12-05 12:30:16 +0200

    chore: Azure/Azurite utility scripts fixes
    Changes:
    
    * Use PUBLIC to refer to the ICON(S) storage
    * Use PRIVATE to refer to the UPLOAD(S) storage
    * Remove references to AZURE_ACCOUNT_KEY, it is no longer used anywhere
    * Remove azure-migrate-from-old-storage.sh script, the data migration is complete
    * Remove references to old unified storage, it no longer exists
    
    Refs: LIIK-854

--- azure-download-icons.sh
+++ azure-download-icons.sh
@@ -3,7 +3,7 @@
 # Utility script for grabbing the icons from the blobstorage and dumping them onto into a local archive.
 # Helpful for archiving icons and grabbing an initial set of icons for populating the local database / azurite storage.
 #
-# Either export AZURE_ACCOUNT_NAME, AZURE_BLOBSTORAGE_CONTAINER_ICONS and AZURE_SAS_TOKEN_ICON or run
+# Either export AZURE_ACCOUNT_NAME, AZURE_BLOBSTORAGE_CONTAINER_PUBLIC and AZURE_SAS_TOKEN_PUBLIC or run
 # source .prepare-azure-scripts.{test,stag,prod}
 # before running this script with those definitions before running this script
 
@@ -12,14 +12,9 @@ set -eu
 DOWNLOAD_DIR="./download-batch-icons"
 mkdir -p "$DOWNLOAD_DIR"
 
-# PRE-STORAGE-MIGRATION WORKAROUNDS
-export AZURE_BLOBSTORAGE_CONTAINER_ICONS="$AZURE_BLOBSTORAGE_CONTAINER_OLD"
-export AZURE_SAS_TOKEN_ICON="$AZURE_SAS_TOKEN_OLD"
-
 az storage blob download-batch \
-    --dryrun \
     --account-name="$AZURE_ACCOUNT_NAME" \
-    --source="$AZURE_BLOBSTORAGE_CONTAINER_ICONS" \
-    --sas-token="$AZURE_SAS_TOKEN_ICON" \
+    --source="$AZURE_BLOBSTORAGE_CONTAINER_PUBLIC" \
+    --sas-token="$AZURE_SAS_TOKEN_PUBLIC" \
     --destination="$DOWNLOAD_DIR" \
     --pattern="icons/*"
