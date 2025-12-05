3afc1c702b75e5cd0710cf503a706804a4b5ed60
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2025-12-05 12:30:16 +0200

    chore: Azure/Azurite utility scripts fixes
    Changes:
    
    * Use PUBLIC to refer to the ICON(S) storage
    * Use PRIVATE to refer to the UPLOAD(S) storage
    * Remove references to AZURE_ACCOUNT_KEY, it is no longer used anywhere
    * Remove azure-migrate-from-old-storage.sh script, the data migration is complete
    * Remove references to old unified storage, it no longer exists
    
    Refs: LIIK-854

--- azure-download-files.sh
+++ azure-download-files.sh
@@ -3,7 +3,7 @@
 # Utility script for grabbing the upload files from the blobstorage and dumping them onto a local archive.
 # Helpful for archiving files and grabbing an initial set of icons for populating the local database / azurite storage.
 #
-# Either export AZURE_ACCOUNT_NAME, AZURE_BLOBSTORAGE_CONTAINER_UPLOADS and AZURE_SAS_TOKEN_UPLOADS or run
+# Either export AZURE_ACCOUNT_NAME, AZURE_BLOBSTORAGE_CONTAINER_PRIVATE and AZURE_SAS_TOKEN_PRIVATE or run
 # source .prepare-azure-scripts.{test,stag,prod}
 # before running this script with those definitions before running this script
 
@@ -12,16 +12,10 @@ set -eu
 DOWNLOAD_DIR="./download-batch-planfiles"
 mkdir -p "$DOWNLOAD_DIR"
 
-# PRE-STORAGE-MIGRATION WORKAROUNDS
-export AZURE_BLOBSTORAGE_CONTAINER_UPLOADS="$AZURE_BLOBSTORAGE_CONTAINER_OLD"
-export AZURE_SAS_TOKEN_UPLOADS="$AZURE_SAS_TOKEN_OLD"
-
-
 az storage blob download-batch \
-    --dryrun \
     --account-name="$AZURE_ACCOUNT_NAME" \
-    --source="$AZURE_BLOBSTORAGE_CONTAINER_UPLOADS" \
-    --sas-token="$AZURE_SAS_TOKEN_UPLOADS" \
+    --source="$AZURE_BLOBSTORAGE_CONTAINER_PRIVATE" \
+    --sas-token="$AZURE_SAS_TOKEN_PRIVATE" \
     --destination="$DOWNLOAD_DIR" \
     --pattern="planfiles/*"
 
@@ -29,10 +23,9 @@ DOWNLOAD_DIR="./download-batch-realfiles"
 mkdir -p "$DOWNLOAD_DIR"
 
 az storage blob download-batch \
-    --dryrun \
     --account-name="$AZURE_ACCOUNT_NAME" \
-    --source="$AZURE_BLOBSTORAGE_CONTAINER_UPLOADS" \
-    --sas-token="$AZURE_SAS_TOKEN_UPLOADS" \
+    --source="$AZURE_BLOBSTORAGE_CONTAINER_PRIVATE" \
+    --sas-token="$AZURE_SAS_TOKEN_PRIVATE" \
     --destination="$DOWNLOAD_DIR" \
     --pattern="realfiles/*"
 
