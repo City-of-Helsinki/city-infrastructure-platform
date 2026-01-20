ad4b52ae629277484f97042f61c447c3b4c91d2d
Janne Forsman <janne.forsman@vincit.fi>
2026-01-20 12:45:02 +0200

    fix: MaintenanceMode application name was not localizable
    Refs: LIIK-886

--- maintenance_mode/apps.py
+++ maintenance_mode/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class MaintenanceModeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "maintenance_mode"
-    verbose_name = "Maintenance Mode"
+    verbose_name = _("Maintenance Mode")
