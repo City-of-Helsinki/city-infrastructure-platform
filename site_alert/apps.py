5a488e6fe67c6c556c0973a66e36ca9df5a2481a
Janne Forsman <janne.forsman@vincit.fi>
2026-01-20 12:55:27 +0200

    fix: SiteAlert application and model was missing internationalization
    Refs: LIIK-886

--- site_alert/apps.py
+++ site_alert/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class SiteAlertConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "site_alert"
-    verbose_name = "Site Alerts"
+    verbose_name = _("Site Alerts")
