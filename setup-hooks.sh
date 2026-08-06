42d24f3d50ade2a40d79083591a13e04fed2b8be
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2026-08-06 09:34:31 +0300

    chore: Add git hook for pre-push stage
    Refs: LIIK-991

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # filepath: setup-hooks.sh
 cp hooks/commit-msg .git/hooks/commit-msg
+cp hooks/pre-push .git/hooks/pre-push
 chmod +x .git/hooks/commit-msg
+chmod +x .git/hooks/pre-push
 echo "Git hooks installed successfully!"
